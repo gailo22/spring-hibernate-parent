5de17102da9d17783d396cc4d0d2f60720221d6b
gailo22 <gailo22@gmail.com>
2012-06-09 14:07:51 +0800

    added sh-common project

---
+++ sh-common/src/main/java/common/money/PercentageUserType.java
@@ -0,0 +1,63 @@
+package common.money;
+
+import java.math.BigDecimal;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Types;
+
+import org.hibernate.HibernateException;
+import org.hibernate.engine.spi.SessionImplementor;
+
+import common.repository.ImmutableValueUserType;
+
+/**
+ * A Hibernate user type for the Percentage type. This class enables Hibernate to map a Percentage object to and from a
+ * double column type in a database.
+ * @see Percentage
+ */
+public class PercentageUserType extends ImmutableValueUserType {
+
+	public Class returnedClass() {
+		return Percentage.class;
+	}
+
+	public int[] sqlTypes() {
+		return new int[] { Types.NUMERIC };
+	}
+
+	@Override
+	public Object nullSafeGet(ResultSet rs, String[] names, Object owner) throws HibernateException, SQLException {
+		BigDecimal value = rs.getBigDecimal(names[0]);
+		if (value == null) {
+			return null;
+		} else {
+			return new Percentage(value);
+		}
+	}
+	
+	@Override
+	public void nullSafeSet(PreparedStatement ps, Object value, int index) throws HibernateException, SQLException {
+		if (value == null) {
+			ps.setNull(index, Types.NUMERIC);
+		} else {
+			Percentage p = (Percentage) value;
+			ps.setBigDecimal(index, p.asBigDecimal());
+		}
+	}
+
+	@Override
+	public Object nullSafeGet(ResultSet arg0, String[] arg1,
+			SessionImplementor arg2, Object arg3) throws HibernateException,
+			SQLException {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public void nullSafeSet(PreparedStatement arg0, Object arg1, int arg2,
+			SessionImplementor arg3) throws HibernateException, SQLException {
+		// TODO Auto-generated method stub
+		
+	}
+}
